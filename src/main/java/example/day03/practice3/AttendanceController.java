bb5bda3aba942c17320645c1ffeffcaad80acc90
박소영 <so0mile02@gmail.com>
2026-02-26 18:16:08 +0900

    260226 오후

--- src/main/java/example/day03/practice3/AttendanceController.java
+++ src/main/java/example/day03/practice3/AttendanceController.java
@@ -1,34 +1,38 @@
 package example.day03.practice3;
 
-import org.springframework.boot.jackson.autoconfigure.JacksonProperties;
-import org.springframework.web.bind.annotation.*;
 
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
 import java.util.List;
 
-import static org.apache.logging.log4j.message.MapMessage.MapFormat.JSON;
 
 @RestController
 @RequestMapping("/attendance")
 public class AttendanceController {
 
-    // 1. 출석 등록
+    // 1. 출석 등록 http://localhost:8080/attendance , body :
     @PostMapping
-    public boolean 출석등록(){
-        System.out.println("AttendanceController.POST");
+    // "ano" 생략시 DTO에서 Int -> Integer 래퍼 클래스
+    public boolean 출석등록(@RequestBody AttendanceDto attendanceDto){
+        System.out.println("AttendanceController.출석등록"); // 확인 용도
         return true;
     }
 
-    // 2. 출석 전체 조회
+    // 2. 출석 전체 조회 http://localhost:8080/attendance
     @GetMapping
     public List<AttendanceDto> 출석전체조회(){
-        System.out.println("AttendanceController.GET");
+        List<AttendanceDto> list = new ArrayList<>();
+        list.add( new AttendanceDto(1,"박진감","2026-02-26","출석"));
+        list.add( AttendanceDto.builder().status("출석").studentName("박소영").date("2026-02-26").ano(1).build());
+        System.out.println("AttendanceController.출석전체조회");
         return list;
     }
 
-    // 3. 출석 개별 조회
+    // 3. 출석 개별 조회 http://localhost:8080/attendance
     @GetMapping("/detail")
     public AttendanceDto 출석개별조회(){
+        AttendanceDto attendanceDto = new AttendanceDto();
         System.out.println("AttendanceController.출석개별조회");
-        return
+        return attendanceDto;
     }
 }
--- src/main/java/example/day03/practice3/AttendanceDto.java
+++ src/main/java/example/day03/practice3/AttendanceDto.java
@@ -2,14 +2,14 @@ package example.day03.practice3;
 
 import lombok.*;
 
-    @Data // getter setter toString
-    @Builder // 빌더패턴(생성자 대신에 사용
-    @NoArgsConstructor @AllArgsConstructor // 생성자
-    public class AttendanceDto {
-        private int ano;
-        private String studentName;
-        private String date;
-        private String status;
+@Data // getter setter toString
+@Builder // 빌더패턴(생성자 대신에 사용
+@NoArgsConstructor @AllArgsConstructor // 생성자
+public class AttendanceDto {
+    private Integer ano;
+    private String studentName;
+    private String date;
+    private String status;
 
-    }
+}
 
