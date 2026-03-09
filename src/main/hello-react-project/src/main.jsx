31bf2bf7c39f01a82a33ceb921d18277073d5b4b
박소영 <so0mile02@gmail.com>
2026-03-09 17:18:22 +0900

    0309

--- src/main/hello-react-project/src/main.jsx
+++ src/main/hello-react-project/src/main.jsx
@@ -2,8 +2,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(<App />)
+// // [1] index.html(싱글페이지) 에서 root 라는 id 갖는 div 호출
+// const root = document.querySelector('#root');
 
-    <App />
+// // [2] root 마크업에 렌더링(render)
+// createRoot( root ).render(<h1>안녕하세요!</h1>)
+// // vs. root.innerHTML = "<h1>안녕하세요!</h1>";
 
-)
--- src/main/java/example/day07/practice7/StudentEntity.java
+++ src/main/java/example/day07/practice7/StudentEntity.java
@@ -14,11 +14,6 @@ public class StudentEntity extends BaseTime {
     private Integer studentId; // 학생번호 // PK
     private String studentName; // 학생명
 
-    // 단방향
-    @ManyToOne // 다수가 하나에게 , 1:M , 하나의 과정에 여러 학생 참조
-    @JoinColumn( name = "courseId" )
-    private CourseEntity courseEntity;
-
     // 양방향
     @OneToMany( mappedBy = "studentEntity" ) // 하나가 다수에게 , 1 : M
     @ToString.Exclude // 순환참조 방지
